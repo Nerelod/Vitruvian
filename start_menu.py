0f76ed72e3cbb971381d1bac4a5e55e124bab675
Daniel Railic <vitruviandan@Daniels-MacBook-Pro.local>
2019-08-23 07:02:31 -0400

    added more options to menu

--- start_menu.py
+++ start_menu.py
@@ -1,5 +1,7 @@
 import os
+import glob
 from termcolor import colored, cprint
+
 
 
 print("                          .%@@@%                           ")
@@ -40,6 +42,7 @@ while vitruvianIsActive == True:
     print("[1] Quit")
     print("[2] Add Password")
     print("[3] Delete Password")
+    print("[4] List Passwords")
     option = input()
     if(option.isdigit()):
         option = int(option)
@@ -50,9 +53,11 @@ while vitruvianIsActive == True:
         vitruvianIsActive = False
     elif option == 2:
         nameoffile = input("What is this password for? ")
-        os.system("touch ~/Documents/vitruvian/" + nameoffile + ".txt")
+        os.system("touch ~/Documents/vitruvian/Passwords/" + nameoffile + ".txt")
         password = input("What is the password for " + nameoffile + "? ")
-        os.system("echo " + password + " >> ~/Documents/vitruvian/" + nameoffile + ".txt")
+        os.system("echo " + password + " >> ~/Documents/vitruvian/Passwords/" + nameoffile + ".txt")
     elif option == 3:
         filetodelete = input("what password do you want to delete? ")
-        os.system("rm ~/Documents/vitruvian/" + filetodelete + ".txt")
+        os.system("rm ~/Documents/vitruvian/Passwords/" + filetodelete + ".txt")
+    elif option == 4:
+        print(os.listdir(os.path.expanduser("~/Documents/vitruvian/Passwords")))
